214097f07439956a00e4d3f207142e122ef74526
daehan <dh16931@gmail.com>
2022-01-31 20:26:53 +0900

    test: check params(size, page, date)

--- server/src/api/patent/patent.test.ts
+++ server/src/api/patent/patent.test.ts
@@ -1,9 +1,54 @@
 import {agent as request} from 'supertest';
 import app from "../../app"
 
-describe('성공시', ()=> {
+describe('Get /patents', ()=> {
     it('상태 코드 200을 반환한다.', async ()=> {
         const res = await request(app).get('/patents');
-        expect(res.status).toEqual(200);
+        expect(res.status).toBe(200);
+    });
+
+    it('return given size number of patents', async ()=> {
+        const size = 2
+        const res = await request(app).get(`/patents?size=${size}`);
+        expect(res.body).toHaveLength(size);
+    });
+    
+    it('return given size number of patents', async ()=> {
+        const size = 2
+        const res = await request(app).get(`/patents?size=${size}`);
+        expect(res.body).toHaveLength(size);
+    });
+})
+
+
+describe('Get /patents wrong params', ()=> {
+    it('return 400 for wrong size type', async ()=> {
+        const res = await request(app).get('/patents?size=size');
+        expect(res.status).toBe(400);
+    });
+    
+    it('return 400 for wrong page type', async ()=> {
+        const res = await request(app).get('/patents?page=page');
+        expect(res.status).toBe(400);
+    });
+    
+    it('return 400 for wrong adStartDate', async ()=> {
+        const res = await request(app).get('/patents?adStartDate=2022.12.21');
+        expect(res.status).toBe(400);
+    });
+    
+    it('return 400 for wrong adEndDate', async ()=> {
+        const res = await request(app).get('/patents?adEndDate=2022.12.21');
+        expect(res.status).toBe(400);
+    });
+    
+    it('return 400 for wrong gdStartDate', async ()=> {
+        const res = await request(app).get('/patents?gdStartDate=2022.12.21');
+        expect(res.status).toBe(400);
+    });
+    
+    it('return 400 for wrong gdEndDate', async ()=> {
+        const res = await request(app).get('/patents?gdEndDate=2022.12.21');
+        expect(res.status).toBe(400);
     });
 })
