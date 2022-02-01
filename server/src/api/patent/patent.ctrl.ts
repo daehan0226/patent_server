adafce4c7238cef4c8468b9df2389e3f365da5b5
daehan <dh16931@gmail.com>
2022-02-01 16:38:24 +0900

    feat: get patent by id endpoint

--- server/src/api/patent/patent.ctrl.ts
+++ server/src/api/patent/patent.ctrl.ts
@@ -1,9 +1,8 @@
 import { Request, Response } from 'express';
+import MongoSingleton from '../../services/db.service';
 import {genMomentDate} from '../../utils/dateHelpers';
 
-let patents = [...new Array(100)].map(()=> {
-    return {"title": "a"}});
-
+const conllection = "patent";
 const defaultSizeStr = "10";
 const defaultPageStr = "1";
 const defaultAdStartDate = "20210101";
@@ -11,7 +10,7 @@ const defaultAdEndDate = "20211231";
 const defaultGdStartDate = "20210101";
 const defaultGdEndDate = "20211231";
 
-interface Params {
+interface IGetAllQuery {
     size: string;
     page: string;
     adStartDate: string;   
@@ -21,23 +20,39 @@ interface Params {
 }
 
 
-const index = function (req:Request<{},{},{},Params>, res: Response, next: Function) {    
+const getAll = async function (req:Request<{},{},{},IGetAllQuery>, res: Response, next: Function) {    
     req.query.size = req.query.size || defaultSizeStr;
     req.query.page = req.query.page || defaultPageStr;
     const size = parseInt(req.query.size, 10);
     const page = parseInt(req.query.page, 10);
     if (Number.isNaN(size) || Number.isNaN(page)) {
-        res.status(400).end()
+        return res.status(400).end()
     }
-
     const adStartDate = genMomentDate({strDate:req.query.adStartDate, defaultDate: defaultAdStartDate})
     const adEndDate = genMomentDate({strDate:req.query.adStartDate, defaultDate: defaultAdEndDate});
     const gdStartDate = genMomentDate({strDate:req.query.adStartDate, defaultDate: defaultGdStartDate});
     const gdEndDate = genMomentDate({strDate:req.query.adStartDate, defaultDate: defaultGdEndDate});
-
-    res.status(200).json(patents.slice(0,size));
+    
+    const patentDb = new MongoSingleton(conllection)
+    const result = await patentDb.find(size)
+    return res.status(200).json(result).end();
 };
 
+interface IGetOneParams {
+    _id: string;
+}
+
+const getById = async function (req:Request<IGetOneParams,{},{},{}>, res: Response, next: Function) {
+    const patentDb = new MongoSingleton(conllection)
+    const result = await patentDb.findById(req.params._id)
+    if (result) {
+        return res.status(200).json(result).end();
+    }
+    return res.status(400).end();
+};
+
+
 export {
-    index
+    getAll,
+    getById
 }
