a1aab3ed3c17652fbcd2bb30d6815813ec07c980
daehan <dh16931@gmail.com>
2022-02-01 10:13:57 +0900

    doc: add env variables for jest and add types

--- server/jest.config.ts
+++ server/jest.config.ts
@@ -4,6 +4,7 @@ export default {
     moduleFileExtensions: ["js", "jsx", "ts", "tsx", "json", "node"],
 
     roots: ["<rootDir>/src"],
+    setupFiles: ["<rootDir>/jest/setEnvVars.ts"],
 
     testMatch: ['**/?(*.)+(spec|test).ts'],
     transform: {
