403822d636d50dc1923858e6ea4802eba8fac50a
daehan <dh16931@gmail.com>
2022-02-05 17:46:25 +0900

    refactor: set size page value function

---
+++ server/src/utils/numberHelpers.test.ts
@@ -0,0 +1,13 @@
+import {convertInt} from "./numberHelpers";
+
+describe('convert given value to number type', ()=> {
+    it('convert given value to number when not isNaN', ()=> {
+      const result = convertInt({value:"10", defaultValue:1});
+      expect(result).toBe(10)
+    });
+
+    it('return defaultValue when given is isNaN', ()=> {
+        const result = convertInt({value:"Ten", defaultValue:5});
+        expect(result).toBe(5)
+      });
+})
