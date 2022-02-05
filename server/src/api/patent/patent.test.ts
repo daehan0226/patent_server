06db4e2dc20548b1d345c0647f443612483367a9
daehan <dh16931@gmail.com>
2022-02-05 22:49:04 +0900

    test: add mongodb memory server for test

--- server/src/api/patent/patent.test.ts
+++ server/src/api/patent/patent.test.ts
@@ -1,53 +1,69 @@
 import {agent as request} from 'supertest';
+import moment from "moment";
 import app from "../../app"
+import MongoSingleton from '../../services/db.service';
 
-describe('Get /patents', ()=> {
-    it('return 200', async ()=> {
-        const res = await request(app).get('/patents?size=1');
-        expect(res.status).toBe(200);
-    });
-
-    it('return given size number of patents', async ()=> {
-        const size = 8
-        const res = await request(app).get(`/patents?size=${size}`);
-        expect(res.body).toHaveLength(size);
+describe('test endpoints after insert dummy patetns', ()=> {
+    beforeAll(async () => {
+        const db = new MongoSingleton("patent_test", "patent")
+        const tempOne = [ ...Array(8).keys() ].map( i => {return{ patent_date: moment(`20210${i+2}0${i+1}`).toDate()}})
+        const tempSecond = [ ...Array(7).keys() ].map( i => {return{ patent_date: moment(`20210${i+3}0${i+2}`).toDate()}})
+        const patents = [...tempOne, ...tempSecond]
+        const result = await db.insertMany(patents)
     });
     
-    it('return default lenth for wrong size type', async ()=> {
-        const defaultSize= 10;
-        const size = 'as'
-        const res = await request(app).get(`/patents?size=${size}`);
-        expect(res.body).toHaveLength(defaultSize);
-    });
-
-    it('return 200 for wrong page type(return with default page)', async ()=> {
-        const page = 'sd'
-        const res = await request(app).get(`/patents?size=1&page=${page}`);
-        expect(res.status).toBe(200);
-    });
-
-    it('return default size number of patents when size is not provided', async ()=> {
-        const res = await request(app).get(`/patents`);
-        expect(res.body).toHaveLength(10);
-    });
-})
-
-
-describe('Get /patents/_id', ()=> {
-    it('return patent by _id value', async ()=> {
-        const _id = "61fe1a3fcee8c6a943c6ec16"
-        const res = await request(app).get(`/patents/${_id}`);
-        expect(res.body).toHaveProperty("_id", _id);
-    });
-    it('retturn 404 for not found', async ()=> {
-        const res = await request(app).get(`/patents/fakeidstring`);
-        expect(res.status).toBe(404);
-    });
-})
-
-describe('Get /patents/random', ()=> {
-    it('return random patents', async ()=> {
-        const res = await request(app).get('/patents/random?size=1');
-        expect(res.status).toBe(200);
-    });
+    describe('Get /patents', ()=> {
+        it('return 200', async ()=> {
+            const res = await request(app).get('/patents?size=1');
+            expect(res.status).toBe(200);
+        });
+    
+        it('return given size number of patents', async ()=> {
+            const size = 5
+            const res = await request(app).get(`/patents?size=${size}`);
+            expect(res.body).toHaveLength(size);
+        });
+        
+        it('return default lenth for wrong size type', async ()=> {
+            const defaultSize= 10;
+            const size = 'as'
+            const res = await request(app).get(`/patents?size=${size}`);
+            expect(res.body).toHaveLength(defaultSize);
+        });
+    
+        it('return 200 for wrong page type(return with default page)', async ()=> {
+            const page = 'sd'
+            const res = await request(app).get(`/patents?size=1&page=${page}`);
+            expect(res.status).toBe(200);
+        });
+    
+        it('return default size number of patents when size is not provided', async ()=> {
+            const res = await request(app).get(`/patents`);
+            expect(res.body).toHaveLength(10);
+        });
+    })
+    
+    
+    describe('Get /patents/_id', ()=> {
+        it('return patent by _id value', async ()=> {
+            const db = new MongoSingleton("patent_test", "patent")
+            const result = await db.insertMany([{"test":"Test"}])
+            if (result) {
+                const _id = result.insertedIds[0].toString()
+                const res = await request(app).get(`/patents/${_id}`);
+                expect(res.body).toHaveProperty("_id", _id);
+            }
+        });
+        it('retturn 404 for not found', async ()=> {
+            const res = await request(app).get(`/patents/fakeidstring`);
+            expect(res.status).toBe(404);
+        });
+    })
+    
+    describe('Get /patents/random', ()=> {
+        it('return random patents', async ()=> {
+            const res = await request(app).get('/patents/random?size=1');
+            expect(res.status).toBe(200);
+        });
+    })
 })
