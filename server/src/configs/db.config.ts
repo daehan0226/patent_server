43547aaf5ae47871eb28a8326cc57dce3c4b431a
daehan <dh16931@gmail.com>
2022-02-05 17:45:08 +0900

    chore: update db url key

--- server/src/configs/db.config.ts
+++ server/src/configs/db.config.ts
@@ -3,12 +3,9 @@ dotenv.config()
 
 const env = process.env;
 const db = {
-    host: env.DB_HOST,
-    user: env.DB_USER,
-    password: env.DB_PASSWORD,
     database: env.DB_NAME,
-    port: env.DB_PORT,
-    collection: env.DB_COLLECTION
+    collection: env.DB_COLLECTION,
+    url: env.MONGODB_URL
 };
 
 module.exports = db;
