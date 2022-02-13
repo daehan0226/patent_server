d1f8c02b83f7fa18bd614cdf3008903942ea61b5
daehan <dh16931@gmail.com>
2022-02-13 15:07:30 +0900

    feat: error handler for logger

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,6 +3,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import morganMiddleware from "./lib/morganMiddleware";
 
+import errorHandler from './lib/errorHandler'
 import patent from "./api/patent";
 
 const app = express();
@@ -13,4 +14,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/patents", patent);
+app.use(errorHandler);
+
+
 export default app;
---
+++ server/src/lib/errorHandler.ts
@@ -0,0 +1,11 @@
+
+import { NextFunction, Request, Response } from "express";
+import {
+	StatusCodes,
+} from 'http-status-codes';
+import Logger from '../lib/logger';
+
+export default function errorHandler(err:string, _:Request, res:Response, __: NextFunction) {
+    Logger.error("Error : ", err)
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).send()
+}
