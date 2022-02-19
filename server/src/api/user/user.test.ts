0b0f9dece9a758574d52e269d9d8410476f89bb2
daehan <dh16931@gmail.com>
2022-02-19 15:11:14 +0900

    test: getAll, delete, update user

--- server/src/api/user/user.test.ts
+++ server/src/api/user/user.test.ts
@@ -33,14 +33,46 @@ describe('test user endpoints', ()=> {
     describe('Get /users/:id', ()=> {
         it('return 200 if user exists', async ()=> {
             const name = 'test03'
-            const response = await request(app).post('/users').send({name});
-            const res = await request(app).get(`/users/${response.body.id}`);
+            const newUser = await request(app).post('/users').send({name});
+            const res = await request(app).get(`/users/${newUser.body.id}`);
             expect(res.status).toBe(200);
         });
         it('return 404 for user does not exist', async ()=> {
             const id = '182398134091'
             const res = await request(app).get(`/users/${id}`);
             expect(res.status).toBe(404);
+        });
+    })
+    
+    describe('Delete /users/:id', ()=> {
+        it('return 404 if user does not exist', async ()=> {
+            const id = '182398134091'
+            const res = await request(app).delete(`/users/${id}`);
+            expect(res.status).toBe(404);
+        });
+        it('return 204 after deleting a user', async ()=> {
+            const name = 'test04'
+            const newUser = await request(app).post('/users').send({name});
+            const res = await request(app).delete(`/users/${newUser.body.id}`);
+            expect(res.status).toBe(204);
+        });
+        // 403 for no auth forbidden after adding session features 
+    })
+    
+    describe('update /users/:id', ()=> {
+        it('return 200 if succeeds to change name', async ()=> {
+            const name = 'test05'
+            const newName = 'test06'
+            const newUser = await request(app).post('/users').send({name});
+            const newUserWithNewName = await request(app).put(`/users/${newUser.body.id}`).send({name:newName});
+            expect(newUserWithNewName.body).toHaveProperty("name", newName);
+        });
+    })
+    
+    describe('get all users /users', ()=> {
+        it('return 200 if succeeds to change name', async ()=> {
+            const res = await request(app).get(`/users`);
+            expect(res.status).toBe(200);
         });
     })
 })
