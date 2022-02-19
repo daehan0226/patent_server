123a82bd503c838814eb6e0cba26330888eb5487
daehan <dh16931@gmail.com>
2022-02-19 15:11:29 +0900

    feat: user getAll, delete, update functions and routes

--- server/src/api/user/index.ts
+++ server/src/api/user/index.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express'
-import {getById, create} from "./user.ctrl"
+import {getById, deleteById, create, update, getAll} from "./user.ctrl"
 
 const router = Router();
 
 router.get('/:_id', getById);
+router.delete('/:_id', deleteById);
+router.put('/:_id', update);
 router.post('', create);
+router.get('', getAll);
 
 
 export default router
