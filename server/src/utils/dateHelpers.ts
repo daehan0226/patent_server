deed93d13217e12ddfd92bfdb4f33068ef97318e
daehan <dh16931@gmail.com>
2022-02-21 23:11:03 +0900

    feat: add timezone with offset hours

--- server/src/utils/dateHelpers.ts
+++ server/src/utils/dateHelpers.ts
@@ -4,21 +4,27 @@ interface genDateProps {
   strDate: string;
   defaultDate: string;
   format?: string;
+  tz?: string;
+}
+
+const offset: {[key: string]: number} = {
+  'KST': 9
 }
 
 const genDate = ({
   strDate,
   defaultDate,
   format="YYYYMMDD",
+  tz="KST"
 }:genDateProps
 ): Date => {
   if (strDate) {
-    const date = moment(strDate, format, true)
+    const date = moment(strDate, format, true).add(offset[tz], 'hours')
     if (date.isValid()) {
       return date.toDate();
     } 
   }
-  return moment(defaultDate, format, true).toDate();
+  return moment(defaultDate, format, true).add(offset[tz], 'hours').toDate();
 };
 
 export { genDate };
