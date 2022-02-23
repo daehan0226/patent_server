47cd86571d320a717c1d1408032fa81c1f4fff9f
daehan <dh16931@gmail.com>
2022-02-23 13:23:32 +0900

    style: sequelize log only for dev

--- server/src/database/mysql/index.ts
+++ server/src/database/mysql/index.ts
@@ -1,6 +1,8 @@
 import { Sequelize } from 'sequelize';
 import db from '../../configs/db.config';
 
-const sequelize = new Sequelize(db.mysqlUrl);
+const isDev = process.env.NODE_ENV === 'development';
+
+const sequelize = new Sequelize(db.mysqlUrl, { logging: isDev });
 
 export default sequelize;
